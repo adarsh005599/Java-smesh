066b75887bf4712e218ddaaf640f65d64f6bd352
adarsh005599 <happysingh005599@gmail.com>
2025-08-31 20:35:47 +0530

    rest future pattern

--- adv java/Level-3-Recursion/pattern.java
+++ adv java/Level-3-Recursion/pattern.java
@@ -100,4 +100,10 @@
 // }
 
 
-
+public class pattern{
+    public static void main(String args[]){
+        for (int i=0; i<11; i++){
+            System.out.print("hello " + i);
+        }
+    }
+}
--- besic java/Hello.java
+++ besic java/Hello.java
@@ -2,6 +2,7 @@ class Hello{
     public static void main(String args[]){
         System.out.println("Hello!!");
         System.out.println("Hello202!!");
+        System.out.println("Hello203!!");
         
     }
 }
